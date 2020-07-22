4e77e67f12790ccbfca96d6713fb3581403ef908
Reid Sherman <reidsherman@gmail.com>
2020-07-22 10:11:02 -0700

    Pull output from docker

--- get_output.sh
+++ get_output.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-while true
+i="0"
+while [ $i -lt 10 ] 
 do
 	docker cp e1be5941ab01:/gpt-2/output .
 	git add .
 	git commit -m "Pull output from docker"
 	git push
-	sleep 60
+	i=$[$i+1]
+	echo $i
+	sleep 1800 
 done
